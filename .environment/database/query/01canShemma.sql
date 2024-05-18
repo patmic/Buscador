78fc4648e4c96ba5ccf890eb21c8037aadd3908e
pat_mic <pat_mic@hotmail.com>
2024-05-18 18:12:43 -0500

    Se agrega borrado logicos a las tablas

--- .environment/database/query/01canShemma.sql
+++ .environment/database/query/01canShemma.sql
@@ -1,11 +1,27 @@
--- https: //www.red-gate.com/simple-talk/databases/sql-server/learn/understanding-full-text-indexing-in-sql-server/
--- agegar campo estado a las tablas para borrado lógico
--- agregar constrains para borrados lógicos
---   
+/*-----------------------------------------------------------------------------------------\
+|         2©24 Copyright      												Quito		   |
+| Todos los Derechos Reservados                                    República del Ecuador   |
+|------------------------------------------------------------------------------------------|
+| Este código está protegido por las leyes y tratados internacionales de derechos de autor |
+| por lo que el uso, reproducción o distribución del código total o parcial no autorizada  |
+| se atendrá a las sanciones contempladas en la ley con su máximo rigor.                   |
+\------------------------------------------------------------------------------------------/
+	- Developer		: patmic										 
+	- Descripction  : crea las entidades de base de datos
+	- Validate		: [ ] Stress o Stress concurrente       [ ] ETL       
+                      [ ] Quality Data                      [X] Log & Audit   
+                      [x] Standart Code 
+\*----------------------------------------------------------------------------------------*/
 
 USE CAN_DB;
 GO
 
+IF OBJECT_ID('UsuarioEndpointPermiso', 'U') IS NOT NULL
+    DROP TABLE UsuarioEndpointPermiso;
+GO
+IF OBJECT_ID('DataLakePersona', 'U') IS NOT NULL
+    DROP TABLE DataLakePersona;
+GO
 IF OBJECT_ID('DataLakeOrganizacion', 'U') IS NOT NULL
     DROP TABLE DataLakeOrganizacion;
 GO
@@ -15,7 +31,9 @@ GO
 IF OBJECT_ID('HomologacionEsquema', 'U') IS NOT NULL
     DROP TABLE HomologacionEsquema;
 GO
-
+IF OBJECT_ID('EndPointWeb', 'U') IS NOT NULL
+    DROP TABLE EndPointWeb;
+GO
 IF OBJECT_ID('WebSiteLog', 'U') IS NOT NULL
     DROP TABLE WebSiteLog;
 GO
@@ -25,9 +43,7 @@ GO
 IF OBJECT_ID(N'Persona', N'U') IS NOT NULL
     DROP TABLE Persona;
 GO
-IF OBJECT_ID('UsuarioEndpointPermiso', 'U') IS NOT NULL
-    DROP TABLE UsuarioEndpointPermiso;
-GO
+
 IF OBJECT_ID('Usuario', 'U') IS NOT NULL
     DROP TABLE Usuario;
 GO
@@ -37,72 +53,64 @@ GO
 IF OBJECT_ID('Homologacion', 'U') IS NOT NULL
     DROP TABLE Homologacion;
 GO
-
 IF OBJECT_ID('HomologacionEsquema', 'U') IS NOT NULL
     DROP TABLE HomologacionEsquema;
 GO
 
 CREATE TABLE Usuario (
-    IdUsuario					INT IDENTITY(1,1) PRIMARY KEY
-    ,Email						NVARCHAR(100) NOT NULL
-    ,Nombre						NVARCHAR(500) NOT NULL
-    ,Apellido					NVARCHAR(500)
-	,Telefono					nvarchar(20)
-    ,Rol						NVARCHAR(20)	NOT NULL DEFAULT('USER')
-    ,Clave						NVARCHAR(MAX)	NOT NULL
-    ,FechaCrea				    DATETIME	NOT NULL DEFAULT(GETDATE())
-    ,FechaModifica			    DATETIME	NOT NULL DEFAULT(GETDATE())  
-    ,IdUserCrea				    INT			NOT NULL DEFAULT(0)
-    ,IdUserModifica			    INT			NOT NULL DEFAULT(0)
-    ,CONSTRAINT UK_Email	UNIQUE (Email)
-    ,CONSTRAINT CK_Rol		CHECK (Rol IN ('ADMIN', 'USER'))
-);
+    IdUsuario				INT IDENTITY(1,1) 
+    ,Email					NVARCHAR(100) NOT NULL
+    ,Nombre					NVARCHAR(500) NOT NULL
+    ,Apellido				NVARCHAR(500)
+	,Telefono				nvarchar(20)
+    ,Clave					NVARCHAR(MAX)	NOT NULL
+    ,Rol					NVARCHAR(20)	NOT NULL DEFAULT('USER')
+	,Estado					NVARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion			DATETIME	NOT NULL DEFAULT(GETDATE())
+    ,FechaModifica			DATETIME	NOT NULL DEFAULT(GETDATE())  
+    ,IdUserCreacion			INT			NOT NULL DEFAULT(0)
+    ,IdUserModifica			INT			NOT NULL DEFAULT(0)
+
+    ,CONSTRAINT PK_U_IdUsuario	PRIMARY KEY CLUSTERED (IdUsuario)  
+    ,CONSTRAINT UK_U_Email		UNIQUE (Email)
+    ,CONSTRAINT CK_U_Rol		CHECK  (Rol IN ('ADMIN', 'USER'))
+    ,CONSTRAINT CK_U_Estado		CHECK  (Estado IN ('A', 'X'))
+)
 GO
 
-CREATE TABLE Endpoint (
-    IdEndpoint			INT IDENTITY(1,1) PRIMARY KEY,
-    Nombre				NVARCHAR(100) NOT NULL,
-    Url					NVARCHAR(MAX)
-    ,FechaCrea				    DATETIME	NOT NULL DEFAULT(GETDATE())
-    ,FechaModifica			    DATETIME	NOT NULL DEFAULT(GETDATE())  
-    ,IdUserCrea				    INT			NOT NULL DEFAULT(0)
-    ,IdUserModifica			    INT			NOT NULL DEFAULT(0)
-    ,CONSTRAINT UK_Nombre UNIQUE (Nombre)
-);
+CREATE TABLE EndPointWeb (
+    IdEndPointWeb			INT IDENTITY(1,1)  
+    ,UrlWeb					NVARCHAR(MAX)
+    ,Nombre					NVARCHAR(100) NOT NULL
+	,Estado					NVARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion			DATETIME	NOT NULL DEFAULT(GETDATE())
+    ,FechaModifica			DATETIME	NOT NULL DEFAULT(GETDATE())  
+    ,IdUserCreacion			INT			NOT NULL DEFAULT(0)
+    ,IdUserModifica			INT			NOT NULL DEFAULT(0)
+    
+	,CONSTRAINT PK_EP_IdEndpoint	PRIMARY KEY CLUSTERED (IdEndPointWeb)  
+    ,CONSTRAINT UK_EP_Nombre		UNIQUE (Nombre)
+    ,CONSTRAINT CK_EP_Estado		CHECK  (Estado IN ('A', 'X'))
+)
 GO
 
 CREATE TABLE UsuarioEndpointPermiso (
-    IdUsuarioEndpointPermiso     INT IDENTITY(1,1) PRIMARY KEY
-    ,IdUsuario                   INT NOT NULL DEFAULT(0)
-    ,IdEndpoint                  INT NOT NULL DEFAULT(0)
-    ,Accion                      NVARCHAR(10) NOT NULL
-    ,FechaCrea				     DATETIME	NOT NULL DEFAULT(GETDATE())
-    ,FechaModifica			     DATETIME	NOT NULL DEFAULT(GETDATE())  
-    ,IdUserCrea				     INT			NOT NULL DEFAULT(0)
-    ,IdUserModifica			     INT			NOT NULL DEFAULT(0)
-    ,CONSTRAINT UK_Accion        CHECK (Accion IN ('GET', 'POST', 'PUT', 'DELETE'))
-    ,CONSTRAINT FK_IdUsuario     FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario)
-    ,CONSTRAINT FK_IdEndpoint    FOREIGN KEY (IdEndpoint) REFERENCES Endpoint(IdEndpoint)
-);
-GO
+    IdUsuarioEndpointPermiso    INT IDENTITY(1,1) 
+    ,IdUsuario                  INT NOT NULL DEFAULT(0)
+    ,IdEndPointWeb              INT NOT NULL DEFAULT(0)
+    ,Accion                     NVARCHAR(10) NOT NULL
+	,Estado						NVARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion				DATETIME	NOT NULL DEFAULT(GETDATE())
+    ,FechaModifica			    DATETIME	NOT NULL DEFAULT(GETDATE())  
+    ,IdUserCreacion				INT			NOT NULL DEFAULT(0)
+    ,IdUserModifica			    INT			NOT NULL DEFAULT(0)
 
---CREATE TABLE Sinonimo (
---    SinonimoId      INT IDENTITY(1,1) NOT NULL CONSTRAINT [PK_SinonimoId] PRIMARY KEY CLUSTERED ,
---    Palabra         NVARCHAR(100),
---    Sinonimo        NVARCHAR(100)
---);
-CREATE TABLE Persona (
-    PersonaId				INT IDENTITY(1,1) NOT NULL 
-    ,Nombre					NVARCHAR(500)   NOT NULL  
-    ,Direccion				NVARCHAR(1000)  NOT NULL  
-    ,Celular				NVARCHAR(100)   NOT NULL  
-    ,Fecha					NVARCHAR(100)   NOT NULL 
-    ,FechaCrea				DATETIME	NOT NULL DEFAULT(GETDATE())
-    ,FechaModifica			DATETIME	NOT NULL DEFAULT(GETDATE())  
-    ,IdUserCrea				INT			NOT NULL DEFAULT(0)
-    ,IdUserModifica			INT			NOT NULL DEFAULT(0) 
-    ,CONSTRAINT      [PK_PersonaId] PRIMARY KEY CLUSTERED (PersonaId ASC)  
-);
+	,CONSTRAINT PK_UEP_IdUsuarioEndpointPermiso	PRIMARY KEY CLUSTERED (IdUsuarioEndpointPermiso)  
+    ,CONSTRAINT FK_UEP_IdUsuario		FOREIGN KEY (IdUsuario)		REFERENCES Usuario(IdUsuario)
+    ,CONSTRAINT FK_UEP_IdEndPointWeb	FOREIGN KEY (IdEndPointWeb)	REFERENCES EndPointWeb(IdEndPointWeb)
+    ,CONSTRAINT UK_UEP_Accion			CHECK (Accion IN ('GET', 'POST', 'PUT', 'DELETE'))
+    ,CONSTRAINT CK_UEP_Estado			CHECK (Estado IN ('A', 'X'))
+)
 GO
 
 CREATE TABLE Homologacion (
@@ -114,65 +122,89 @@ CREATE TABLE Homologacion (
     ,MostrarWeb				NVARCHAR(90)  NOT NULL
 	,Descripcion			NVARCHAR(200)
     ,InfoExtraJson			NVARCHAR(max) NOT NULL DEFAULT('{}')
-    ,FechaCrea				DATETIME	NOT NULL DEFAULT(GETDATE())
+	,Estado					NVARCHAR(1) NOT NULL DEFAULT('A')
+    ,FechaCreacion			DATETIME	NOT NULL DEFAULT(GETDATE())
     ,FechaModifica			DATETIME	NOT NULL DEFAULT(GETDATE())  
-    ,IdUserCrea				INT			NOT NULL DEFAULT(0)
+    ,IdUserCreacion			INT			NOT NULL DEFAULT(0)
     ,IdUserModifica			INT			NOT NULL DEFAULT(0)  
-	,CONSTRAINT  [PK_IdHomologacion]		PRIMARY KEY CLUSTERED (IdHomologacion) 
-	,CONSTRAINT  [UK_CodigoHomologacion]	UNIQUE  (CodigoHomologacion)
-    ,CONSTRAINT  [CK_InfoExtraJson]         CHECK   ( ISJSON(InfoExtraJson) = 1 )
-);
+	
+	,CONSTRAINT  [PK_H_IdHomologacion]		PRIMARY KEY CLUSTERED (IdHomologacion) 
+	,CONSTRAINT  [UK_H_CodigoHomologacion]	UNIQUE  (CodigoHomologacion)
+    ,CONSTRAINT  [CK_H_InfoExtraJson]         CHECK   (ISJSON(InfoExtraJson) = 1 )
+    ,CONSTRAINT  [CK_H_Estado]				CHECK   (Estado IN ('A', 'X'))
+)
 GO
 
 CREATE TABLE HomologacionEsquema(
 	 IdHomologacionEsquema     	INT IDENTITY(1,1) NOT NULL
-	,CodigoHomologacionEsquema	NVARCHAR(25)  NOT NULL
-	,NombreHomologacionEsquema	NVARCHAR(200) NOT NULL
-    ,HomologacionEsquemaJson	NVARCHAR(max) NOT NULL DEFAULT('{}')
     ,MostrarWebOrden			INT DEFAULT(0) 
     ,MostrarWeb					NVARCHAR(200)  NOT NULL
 	,Descripcion				NVARCHAR(200)
-	,FechaCrea					DATETIME	NOT NULL DEFAULT(GETDATE())
+    ,EsquemaJson				NVARCHAR(max) NOT NULL DEFAULT('{}')
+	,Estado						NVARCHAR(1) NOT NULL DEFAULT('A')
+	,FechaCreacion				DATETIME	NOT NULL DEFAULT(GETDATE())
     ,FechaModifica				DATETIME	NOT NULL DEFAULT(GETDATE())  
-    ,IdUserCrea					INT			NOT NULL DEFAULT(0)
+    ,IdUserCreacion				INT			NOT NULL DEFAULT(0)
     ,IdUserModifica				INT			NOT NULL DEFAULT(0)  
-	,CONSTRAINT  [PK_IdHomologacionEsquema]		PRIMARY KEY CLUSTERED (IdHomologacionEsquema) 
-	,CONSTRAINT  [UK_CodigoHomologacionEsquema]	UNIQUE  (CodigoHomologacionEsquema)
-    ,CONSTRAINT  [CK_HomologacionEsquemaJson]	CHECK   ( ISJSON(HomologacionEsquemaJson) = 1 )
-);
+	,CONSTRAINT  [PK_HE_IdHomologacionEsquema]	PRIMARY KEY CLUSTERED (IdHomologacionEsquema) 
+    ,CONSTRAINT  [CK_HE_EsquemaJson]			CHECK   (ISJSON(EsquemaJson) = 1 )
+    ,CONSTRAINT  [CK_HE_Estado]				CHECK   (Estado IN ('A', 'X'))
+)
 GO
 
 CREATE TABLE DataLake(
     IdDataLake				INT IDENTITY(1,1) NOT NULL
-	,DataTipo				NVARCHAR(15) NOT NULL DEFAULT('ORGANIZACION') 
+	,DataTipo				NVARCHAR(15) NOT NULL DEFAULT('NO_DEFINIDO') 
 	,DataSistemaOrigen		NVARCHAR(15) NOT NULL	
     ,DataSistemaOrigenId	NVARCHAR(10) NOT NULL 
     ,DataSistemaFecha		DATETIME	
+	,Estado					NVARCHAR(1) NOT NULL DEFAULT('A')
     ,DataFechaCarga			DATETIME	 NOT NULL DEFAULT(GETDATE())
-    ,FechaCrea				DATETIME	 NOT NULL DEFAULT(GETDATE())
-    ,FechaModifica			DATETIME	 NOT NULL DEFAULT(GETDATE())  
-	,CONSTRAINT  [PK_IdDataLake]	PRIMARY KEY CLUSTERED (IdDataLake) 
-	,CONSTRAINT  [UK_DataTipo]		CHECK (DataTipo IN ('ORGANIZACION', 'PERSONA'))
-);
+	
+	,CONSTRAINT  [PK_DL_IdDataLake]	PRIMARY KEY CLUSTERED (IdDataLake) 
+	,CONSTRAINT  [UK_DL_DataTipo]		CHECK (DataTipo IN ('ORGANIZACION', 'PERSONA','NO_DEFINIDO'))
+    ,CONSTRAINT  [CK_DL_Estado]		CHECK   (Estado IN ('A', 'X'))
+)
 GO
 
 CREATE TABLE DataLakeOrganizacion(
     IdDataLakeOrganizacion  INT IDENTITY(1,1) NOT NULL
     ,IdHomologacionEsquema	INT NOT NULL  FOREIGN KEY REFERENCES HomologacionEsquema (IdHomologacionEsquema)
     ,IdDataLake				INT NOT NULL  FOREIGN KEY REFERENCES DataLake (IdDataLake)
-    ,DataJsonEsquema        NVARCHAR(max) NOT NULL DEFAULT('{}')
-    ,DataJsonEsquemaExtra   NVARCHAR(max) NOT NULL DEFAULT('{}')
-    ,DataJsonFechaCarga		DATETIME	  NOT NULL DEFAULT(GETDATE())
-    ,FechaCrea				DATETIME	NOT NULL DEFAULT(GETDATE())
-    ,FechaModifica			DATETIME	NOT NULL DEFAULT(GETDATE())  
-	,CONSTRAINT  [PK_IdDataLakeOrganizacion]			PRIMARY KEY CLUSTERED (IdDataLakeOrganizacion) 
-    ,CONSTRAINT  [CK_DataJsonEsquema]		CHECK   ( ISJSON(DataJsonEsquema) = 1 )
-    ,CONSTRAINT  [CK_DataJsonEsquemaExtra]	CHECK   ( ISJSON(DataJsonEsquemaExtra) = 1 )
-);
+    ,DataEsquemaJson        NVARCHAR(max) NOT NULL DEFAULT('{}')
+    ,DataFechaCarga			DATETIME	  NOT NULL DEFAULT(GETDATE())
+    ,FechaCreacion			DATETIME	NOT NULL DEFAULT(GETDATE())
+	,Estado					NVARCHAR(1) NOT NULL DEFAULT('A')
+	
+	,CONSTRAINT  [PK_DKOrganizacion_IdDataLakeOrganizacion]	PRIMARY KEY CLUSTERED (IdDataLakeOrganizacion) 
+    ,CONSTRAINT  [CK_DKOrganizacion_DataEsquemaJson]		CHECK   (ISJSON(DataEsquemaJson) = 1 )
+    ,CONSTRAINT  [CK_DKOrganizacion_Estado]					CHECK   (Estado IN ('A', 'X'))
+)
+GO
+
+CREATE TABLE DataLakePersona(
+    IdDataLakePersona		INT IDENTITY(1,1) NOT NULL
+    ,IdHomologacionEsquema	INT NOT NULL  FOREIGN KEY REFERENCES HomologacionEsquema (IdHomologacionEsquema)
+    ,IdDataLake				INT NOT NULL  FOREIGN KEY REFERENCES DataLake (IdDataLake)
+    ,DataEsquemaJson        NVARCHAR(max) NOT NULL DEFAULT('{}')
+    ,DataFechaCarga			DATETIME	  NOT NULL DEFAULT(GETDATE())
+    ,FechaCreacion			DATETIME	NOT NULL DEFAULT(GETDATE())
+	,Estado					NVARCHAR(1) NOT NULL DEFAULT('A')
+	
+	,CONSTRAINT  [PK_DKPersona_DataLakePersona]	PRIMARY KEY CLUSTERED (IdDataLakePersona) 
+    ,CONSTRAINT  [CK_DKPersona_DataEsquemaJson]	CHECK   (ISJSON(DataEsquemaJson) = 1 )
+    ,CONSTRAINT  [CK_DKPersona_Estado]			CHECK   (Estado IN ('A', 'X'))
+)
 GO
 
 CREATE TABLE WebSiteLog (
-    IdWebSiteLog    bigint primary key identity,
-    log             nvarchar(max)
-);
+    IdWebSiteLog		INT IDENTITY(1,1) NOT NULL
+    ,DataLogJson		NVARCHAR(max)
+	,FechaCreacion		DATETIME	NOT NULL DEFAULT(GETDATE())
+	,Estado				NVARCHAR(1) NOT NULL DEFAULT('A')
+	
+	,CONSTRAINT  [PK_WSL_WebSiteLog]		PRIMARY KEY CLUSTERED (IdWebSiteLog) 
+    ,CONSTRAINT  [CK_WSL_DataEsquemaJson]	CHECK   (ISJSON(DataLogJson) = 1 )
+    ,CONSTRAINT  [CK_WSL_Estado]			CHECK   (Estado IN ('A', 'X'))
+)
 GO
